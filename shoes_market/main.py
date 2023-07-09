868be99a7a8a0aab1efaf30956eca8a5ec91608d
Bakhytbek <baxa.aseke@gmail.com>
2023-07-09 13:14:48 +0600

    Added users router and added middleware

--- shoes_market/main.py
+++ shoes_market/main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+from shoes_market import users, connection, middleware, settings
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hell {name}"}
+app = FastAPI(default_response_class=ORJSONResponse)
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(middleware.JWTMiddleware, secret_key=settings.SECRET_KEY)
+
+api_router = APIRouter(prefix='/api')
+api_router.include_router(users.router)
+
+app.include_router(api_router)
+
+
+@app.on_event('shutdown')
+async def close_redis_connection():
+    connection.Connection.close_redis()
+    await connection.Connection.close_aioredis()
