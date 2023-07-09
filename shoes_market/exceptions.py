ae77f78fef6fb7c8f7130f9484fd50f285e0442f
Bakhytbek <baxa.aseke@gmail.com>
2023-07-09 13:22:37 +0600

    Added exceptions to shoes_market

---
+++ shoes_market/exceptions.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+from fastapi import HTTPException, status
+
+
+class Unauthorized(HTTPException):
+
+    def __init__(
+        self, status_code: int = status.HTTP_401_UNAUTHORIZED,
+        detail: Any = 'Unauthorized',
+        headers: dict | None = None,
+    ) -> None:
+        super().__init__(status_code=status_code, detail=detail, headers=headers)
+
+
+class RedisUrlNotFound(HTTPException):
+
+    def __init__(
+        self, status_code: int = status.HTTP_404_NOT_FOUND,
+        detail: Any = 'Redis url not found',
+        headers: dict | None = None,
+    ) -> None:
+        super().__init__(status_code=status_code, detail=detail, headers=headers)
