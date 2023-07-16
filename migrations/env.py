3fd4b957f9438f9a3a81aec11f0b33a157e4fca7
Bakhytbek <baxa.aseke@gmail.com>
2023-07-16 16:36:39 +0600

    Change base model

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,7 @@ from alembic import context
 
 from shoes_market import models, settings
 from shoes_market.users import models as user_models
+from shoes_market.products import models as product_models
 
 
 # this is the Alembic Config object, which provides
@@ -24,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = models.BaseModel.metadata
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- shoes_market/models.py
+++ shoes_market/models.py
@@ -6,8 +6,13 @@ from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class BaseModel(AsyncAttrs, DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, index=True, default=uuid.uuid4)
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
     created_at: Mapped[dt.datetime] = mapped_column(
         DateTime(timezone=True), default=dt.datetime.now,
     )
