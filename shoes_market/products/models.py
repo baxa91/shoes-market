8b1db595ed25e7bddd3866a80eff6fe6db62cc33
Bakhytbek <baxa.aseke@gmail.com>
2023-07-28 08:49:50 +0600

    Change relationship

--- shoes_market/products/models.py
+++ shoes_market/products/models.py
@@ -28,14 +28,16 @@ class Product(models.BaseModel):
     currency: Mapped[str] = mapped_column(String(4), default='KZT')
     description: Mapped[str] = mapped_column(Text(), nullable=True)
 
-    tags = relationship('Tag', secondary=ProductTag, back_populates='products')
-    images = relationship('ProductImage', back_populates='products')
+    tags = relationship('Tag', secondary=ProductTag, back_populates='products', cascade='all')
+    images = relationship('ProductImage', back_populates='products', cascade='all, delete-orphan')
 
 
 class ProductImage(models.BaseModel):
     __tablename__ = 'product_image'
 
-    product_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('products_product.id'))
+    product_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey('products_product.id', ondelete='CASCADE')
+    )
     image: Mapped[str] = mapped_column(Text())
     is_base: Mapped[bool] = mapped_column(default=False)
     products = relationship('Product', back_populates='images')
