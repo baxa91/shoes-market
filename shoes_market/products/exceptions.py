aac807516fa51fdce5430ee5698dd32e93bcd274
Bakhytbek <baxa.aseke@gmail.com>
2023-07-28 08:50:14 +0600

    Added gettext

--- shoes_market/products/exceptions.py
+++ shoes_market/products/exceptions.py
@@ -1,36 +1,41 @@
+from gettext import gettext as _
+
+
 class PriceNegativeException(ValueError):
 
     def __init__(self, *args):
-        super().__init__('Цена указано не правильно', *args)
+        super().__init__(_('Цена указано не правильно'), *args)
 
 
 class ExistNameException(ValueError):
 
     def __init__(self, *args):
-        super().__init__('Такое тэг уже существует', *args)
+        super().__init__(_('Такой тэг уже существует'), *args)
 
 
 class ImageCountException(ValueError):
 
     def __init__(self, *args):
-        super().__init__('Количество фото превышает допустимого', *args)
+        super().__init__(_('Количество фото превышает допустимого'), *args)
 
 
 class ImageTypeException(ValueError):
 
     def __init__(self, *args):
         super().__init__(
-            'Недопустимый тип файла. Поддерживаются только форматы JPEG, PNG, GIF и WebP', *args)
+            _('Недопустимый тип файла. Поддерживаются только форматы JPEG, PNG, GIF и WebP'),
+            *args
+        )
 
 
 class ImageBaseException(ValueError):
 
     def __init__(self, *args):
         super().__init__(
-            'Главное фото, может быть только одна', *args)
+            _('Главное фото, может быть только одна'), *args)
 
 
 class DoesNotExistsException(ValueError):
 
     def __init__(self, *args):
-        super().__init__('Объект не существует', *args)
+        super().__init__(_('Объект не существует'), *args)
