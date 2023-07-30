78d22e3679ad92ab143a07b56ad2a360df9be423
Bakhytbek <baxa.aseke@gmail.com>
2023-07-30 11:17:53 +0600

    Added delete product image constant

--- shoes_market/products/constants.py
+++ shoes_market/products/constants.py
@@ -3,3 +3,5 @@ from gettext import gettext as _
 
 DELETE_TAG = _('Тэг удален')
 DELETE_PRODUCT = _('Товар удален')
+UPDATE_PRODUCT_IMAGE = _('Картинка изменена')
+DELETE_PRODUCT_IMAGE = _('Картинка удалена')
